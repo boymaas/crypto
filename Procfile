6f8cf35214977a3885a026bfa56430f0890b5e77
Boy Maas <boy.maas@gmail.com>
2013-12-22 18:08:18 +0100

    small tweak in provisioni and procfile

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
 web:            bundle exec rails server -p $PORT
-data_collector: bundle exec crypto_trader_data_collector
-vacuum:         bundle exec crypto_trader_vacuumer
-bot_runner:     bundle exec crypto_trader_bot_runner
--- provision/user-crypto.sh
+++ provision/user-crypto.sh
@@ -5,9 +5,12 @@ user_install_authorized_key 'crypto' 'macbook-pro.pub'
 
 user_sudo_nopasswd 'crypto'
 
-# echo "Install database yaml"
-cp provision/user-crypto/database.yml ~crypto/shared/config/
-chown crypto ~crypto/shared/config/database.yml
+# echo "Install database yaml, for use with mina"
+sudo -u crypto -i <<EOS
+  mkdir -p shared/config
+EOS
+cp provision/user-crypto/database.yml /home/crypto/shared/config/
+chown crypto.crypto /home/crypto/shared/config/database.yml
 
 # echo "Adding environment file for app"
 cp provision/user-crypto/environment ~crypto/environment
