8172d7941ffdb5cee015ac837a9110cbda8bcc9e
Boy Maas <boy.maas@gmail.com>
2013-12-23 23:43:45 +0100

    projected emas

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -8,7 +8,11 @@ class MarketsController < ApplicationController
     @market = CryptoTrader::Model::Market.find(:id => params.fetch(:id))  
     data = @market.trade_stats_dataset.order(:rounded_date).all
 
-    data = data.map {|e| [e.rounded_date.to_i * 1000, e.price_avg.to_f, e.total_sum.to_f] }
+    ema_24 = data.map(&:price_avg).indicator(:ema, 24)
+    ema_48 = data.map(&:price_avg).indicator(:ema, 48)
+
+    data = data.zip(ema_24.zip(ema_48)).flatten
+    data = data.each_slice(3).map {|d,e1,e2| [d.rounded_date.to_i * 1000, d.price_avg.to_f, d.total_sum.to_f, e1, e2] }
 
     # render :json => "callback(#{data.to_json});"
     render :json => data.to_json
