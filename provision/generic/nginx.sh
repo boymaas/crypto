6ab2c243bd7c86a1bf410e7bfa08e826a6ef93b9
Boy Maas <boy.maas@gmail.com>
2013-12-23 13:01:24 +0100

    nginx fix

--- provision/generic/nginx.sh
+++ provision/generic/nginx.sh
@@ -1,11 +1,11 @@
-nxing_install() {
+nginx_install() {
   echo "Installing Nginx"
   apt-get install -y nginx >/dev/null 2>&1
 }
 
 
 nginx_place_config() {
-  rm -f                               /etc/nginx/sites-enabled/default
-  cp $1 /etc/nginx/sites-enabled/$1
+  rm -f /etc/nginx/sites-enabled/default
+  cp $1 /etc/nginx/sites-enabled/$(basename $1)
   service nginx start
 }
