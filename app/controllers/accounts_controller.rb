489115678a741b01cb8dfe60beb08866dd39ab74
Boy Maas <boy.maas@gmail.com>
2013-12-24 03:49:14 +0100

    shows better time, and account page visible

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,9 @@ class AccountsController < ApplicationController
   def index
     @accounts = CryptoTrader::Model::Account.all
   end
+
+  def show
+    @account = CryptoTrader::Model::Account.find(:id => params.fetch(:id))
+    @account_trades = @account.trades_dataset.reverse_order(:timestamp).limit(10)
+  end
 end
