0e16f0607db10eeaa17f09d900d1f122798bb78d
Boy Maas <boy.maas@gmail.com>
2013-12-25 10:31:14 +0100

    portfolio screen for accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,11 @@ class AccountsController < ApplicationController
 
   def show
     @account = CryptoTrader::Model::Account.find(:id => params.fetch(:id))
+
+    market_data_provider = CryptoTrader::MarketDataProvider.new()
+    account_data_provider = CryptoTrader::AccountDataProvider.new(@account)
+    @portfolio = CryptoTrader::Portfolio.new(account_data_provider, market_data_provider)
+
     @account_trades = @account.trades_dataset.reverse_order(:timestamp).limit(10)
   end
 end
