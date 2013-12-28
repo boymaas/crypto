3cee1a198f1901f9065dac83e1b45405ba5b8c2a
Boy Maas <boy.maas@gmail.com>
2013-12-28 17:24:49 +0100

    disabled rach cache store, gives problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,10 +18,10 @@ Crypto::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  config.action_dispatch.rack_cache = {
-    metastore:   'redis://localhost:6379/1/metastore',
-    entitystore: 'redis://localhost:6379/1/entitystore'
-  }
+  # config.action_dispatch.rack_cache = {
+  #   metastore:   'redis://localhost:6379/1/metastore',
+  #   entitystore: 'redis://localhost:6379/1/entitystore'
+  # }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
