16397453405773d9a20f47632587d3d77edb3e42
Boy Maas <boy.maas@gmail.com>
2014-01-04 20:55:50 +0100

    40 results

--- app/controllers/market_states_controller.rb
+++ app/controllers/market_states_controller.rb
@@ -1,7 +1,7 @@
 class MarketStatesController < SecuredController
   def index
     @market = CryptoTrader::Model::Market.find(:id => params.fetch(:market_id))
-    @market_states = @market.states_dataset.reverse_order(:timestamp).extension(:pagination).paginate(params[:page], 20)
+    @market_states = @market.states_dataset.reverse_order(:timestamp).extension(:pagination).paginate(params[:page], 40)
   end
 
   def show
