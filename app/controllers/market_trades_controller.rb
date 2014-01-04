438f52d204cef1b69cc4681fbff391eea0076112
Boy Maas <boy.maas@gmail.com>
2014-01-04 20:58:13 +0100

    market trades 40 results

--- app/controllers/market_trades_controller.rb
+++ app/controllers/market_trades_controller.rb
@@ -2,6 +2,6 @@ class MarketTradesController < SecuredController
   def index
     @market  = CryptoTrader::Model::Market[params[:market_id]]
     analyzed_market = CryptoTrader::AnalyzedMarket.new(@market)
-    @market_trades = analyzed_market.ordered_desc_trades.extension(:pagination).paginate(params[:page], 20)
+    @market_trades = analyzed_market.ordered_desc_trades.extension(:pagination).paginate(params[:page], 40)
   end
 end
